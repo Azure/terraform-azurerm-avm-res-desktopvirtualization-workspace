ed57735fc63c31ccd796234c68b380d129bf67c6
Bas <61052187+BasBerk@users.noreply.github.com>
2025-06-18 07:02:37 +0200

    fix: added Azapi as provider

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.9, < 2.0"
   required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 1.0.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.71, < 5.0.0"
